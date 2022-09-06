49f216702492a80102b008aa92f6e305494cab04
soo <yunsoo0510@daum.net>
2022-09-06 22:59:03 +0900

    [feat] HttpSession.setAttribute(String name, Object value) & getAttribute() 구현

--- src/main/java/webserver/HttpSession.java
+++ src/main/java/webserver/HttpSession.java
@@ -1,9 +1,12 @@
 package webserver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class HttpSession {
     private final String id;
+    private final Map<String, Object> attributes = new HashMap<>();
 
     public HttpSession() {
         this.id = UUID.randomUUID().toString();
@@ -11,5 +14,13 @@ public class HttpSession {
 
     public String getId() {
         return id;
+    }
+
+    public void setAttribute(String name, Object value) {
+        attributes.put(name, value);
+    }
+
+    public Object getAttribute(String name) {
+        return attributes.get(name);
     }
 }
--- src/test/java/webserver/HttpSessionTest.java
+++ src/test/java/webserver/HttpSessionTest.java
@@ -1,19 +1,34 @@
 package webserver;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class HttpSessionTest {
+    private HttpSession session;
+
+    @BeforeEach
+    void init() {
+        session = new HttpSession();
+    }
 
     @Test
     void getId() {
-        HttpSession session = new HttpSession();
-
         // 랜덤함을 증명할 수 있을까?
         UUID.fromString(session.getId());
         assertThatNoException();
+    }
+
+    @Test
+    void setAndGetAttribute() {
+        String value = "soosue";
+        session.setAttribute("id", value);
+
+        Object getValue = session.getAttribute("id");
+        assertThat(value).isEqualTo(getValue);
     }
 }
